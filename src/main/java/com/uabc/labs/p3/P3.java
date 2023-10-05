5ea399349da06bbcd4b8cfd125b2454c1c309949
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2023-10-04 23:35:35 -0700

    Initial commit

---
+++ src/main/java/com/uabc/labs/p3/P3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uabc.labs.p3;
+
+/**
+ *
+ * @author us
+ */
+public class P3 {
+
+    public static void main(String[] args) {
+        UI2 test = new UI2();
+        test.setVisible(true);
+    }
+}
